df1aa8a7c567ec27e98d90142c447cee077a3939
mali84211 <mali84211@gmail.com>
2024-07-24 16:25:51 +0300

    Search Ex

--- C42-G02-ADV01/Helper.cs
+++ C42-G02-ADV01/Helper.cs
@@ -6,32 +6,41 @@ using System.Threading.Tasks;
 
 namespace C42_G02_ADV01
 {
-    internal class Helper
+    internal class Helper<T>
     {
-        public static void SWAP(ref int x,ref int y)
-        {
-            int Temp = x;
-            x = y;
-            y = Temp;
-        }
-        public static void SWAP(ref double x, ref double y)
-        {
-            double Temp = x;
-            x = y;
-            y = Temp;
-        }
-        public static void SWAP(ref point x, ref point y)
-        {
-            point Temp = x;
-            x = y;
-            y = Temp;
-        }
-        //T Salary {  get; set; }
-        public static void SWAP<T>(ref T x, ref T y)
-        {
-            T Temp = x;
-            x = y;
-            y = Temp;
-        }
+        //public static void SWAP(ref int x,ref int y)
+        //{
+        //    int Temp = x;
+        //    x = y;
+        //    y = Temp;
+        //}
+        //public static void SWAP(ref double x, ref double y)
+        //{
+        //    double Temp = x;
+        //    x = y;
+        //    y = Temp;
+        //}
+        //public static void SWAP(ref point x, ref point y)
+        //{
+        //    point Temp = x;
+        //    x = y;
+        //    y = Temp;
+        //}
+        ////T Salary {  get; set; }
+        //public static void SWAP<T>(ref T x, ref T y)
+        //{
+        //    T Temp = x;
+        //    x = y;
+        //    y = Temp;
+        //}
+        //public static int SearchArray(T[]array,T value) {
+
+        //    if (array is not null) {
+        //        for (int i = 0; i < array.Length; i++) { 
+        //        if (array[i] == value) 
+        //                return i;
+        //        }
+        //    }
+        //}
     }
 }
--- C42-G02-ADV01/Program.cs
+++ C42-G02-ADV01/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace C42_G02_ADV01
+﻿using System.Xml.Linq;
+
+namespace C42_G02_ADV01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region
             //int A = 10; int B = 20;
             //Console.WriteLine($"A={ِA}");
             //Console.WriteLine($"B={B}");
@@ -27,9 +30,31 @@
             //Helper<point>.SWAP(ref p1,ref p2);
             //Console.WriteLine(p1);
             //Console.WriteLine(p2);
+            #endregion
+
+            #region V2
+           // int[] Numbers = {4,5,6,9,3,2,8,1,7};
+           //int index= Helper.SearchArray(Numbers, 6);
+           // Console.WriteLine(index);
+
+            //Employee E1=new Employee()
+            //{Id=10,Name="mo",Salary=3000 };
+            //Employee E2 = new Employee()
+            //{ Id = 11,Name = "amr",Salary = 4000 };
+            //if (E1 == E2)
+            //    Console.WriteLine("Equal");
+            //else Console.WriteLine("not Equal");
+
+            //if (E1 Equals (E2))
+            //    Console.WriteLine("Equal");
+            //else Console.WriteLine("not Equal");
 
 
+            #endregion
 
+
+            #region
+            #endregion
         }
     }
 }
