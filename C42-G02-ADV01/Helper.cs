8e6b862975f6769725e6efa1769e67618645ed01
mali84211 <mali84211@gmail.com>
2024-07-24 16:08:44 +0300

    SWAP

---
+++ C42-G02-ADV01/Helper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C42_G02_ADV01
+{
+    internal class Helper
+    {
+        public static void SWAP(ref int x,ref int y)
+        {
+            int Temp = x;
+            x = y;
+            y = Temp;
+        }
+        public static void SWAP(ref double x, ref double y)
+        {
+            double Temp = x;
+            x = y;
+            y = Temp;
+        }
+        public static void SWAP(ref point x, ref point y)
+        {
+            point Temp = x;
+            x = y;
+            y = Temp;
+        }
+        //T Salary {  get; set; }
+        public static void SWAP<T>(ref T x, ref T y)
+        {
+            T Temp = x;
+            x = y;
+            y = Temp;
+        }
+    }
+}
--- C42-G02-ADV01/Program.cs
+++ C42-G02-ADV01/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //int A = 10; int B = 20;
+            //Console.WriteLine($"A={ِA}");
+            //Console.WriteLine($"B={B}");
+            //Helper<int>.SWAP(ref A, ref B);
+            //Console.WriteLine("After Swapping");
+            //Console.WriteLine($"A={ِA}");
+            //Console.WriteLine($"B={B}");
+
+            //double A = 2.2, B = 3.3;
+            //Console.WriteLine($"A={ِA}");
+            //Console.WriteLine($"B={B}");
+            //Helper<double>.SWAP(ref A, ref B);
+            //Console.WriteLine("After Swapping");
+            //Console.WriteLine($"A={ِA}");
+            //Console.WriteLine($"B={B}");
+
+            //point p1 = new point(1, 2);
+            //point p2 = new point(10, 20);
+            //Console.WriteLine(p1);
+            //Console.WriteLine(p2);
+            //Helper<point>.SWAP(ref p1,ref p2);
+            //Console.WriteLine(p1);
+            //Console.WriteLine(p2);
+
+
+
         }
     }
 }
