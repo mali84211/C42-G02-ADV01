7215a3cb4c20e8afc0f3c8f36b81657cc4aec74e
mali84211 <mali84211@gmail.com>
2024-07-24 15:41:44 +0300

    All Ass

--- C42-G02-ADV01/Program.cs
+++ C42-G02-ADV01/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace C42_G02_ADV01
+﻿using static C42_G02_ADV01.Range_T;
+
+namespace C42_G02_ADV01
 {
     internal class Program
     {
-        static void Main(string[] args)
+        #region 1
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            int[] array = { 64, 34, 25, 12, 22, 11, 90 };
+            BubbleSortOptimized(array);
+            Console.WriteLine("Sorted array: " + string.Join(", ", array));
         }
+        static void BubbleSortOptimized(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                bool swapped = false;
+
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+
+                        
+                        swapped = true;
+                    }
+                }
+                if (!swapped)
+                {
+                    break;
+                }
+            }
+        }
+        #endregion
+
+        #region 2
+        public static void Main(string[] args)
+        {
+            Range<int> intRange = new Range<int>(1, 10);
+            Console.WriteLine("Is 5 in range: " + intRange.IsInRange(5)); 
+            Console.WriteLine("Length of range: " + intRange.Length());   
+
+            Range<double> doubleRange = new Range<double>(1.5, 10.5);
+            Console.WriteLine("Is 7.5 in range: " + doubleRange.IsInRange(7.5)); 
+            Console.WriteLine("Length of range: " + doubleRange.Length());
+        }
+        #endregion
     }
 }
---
+++ C42-G02-ADV01/Range T.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C42_G02_ADV01
+{
+    internal class Range_T
+    {
+        public class Range<T> where T : IComparable<T>
+        {
+            public T Minimum { get; private set; }
+            public T Maximum { get; private set; }
+            public Range(T minimum, T maximum)
+            {
+                if (minimum.CompareTo(maximum) > 0)
+                {
+                    throw new ArgumentException("Minimum should be less than or equal to Maximum");
+                }
+
+                Minimum = minimum;
+                Maximum = maximum;
+            }
+            public bool IsInRange(T value)
+            {
+                return value.CompareTo(Minimum) >= 0 && value.CompareTo(Maximum) <= 0;
+            }
+
+            public T Length()
+            {
+                
+                dynamic min = Minimum;
+                dynamic max = Maximum;
+                return max - min;
+            }
+        }
+
+    }
+}
